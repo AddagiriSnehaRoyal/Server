ca9229325325814ba74d9ff318d50577bca9cb09
Addagiri Sneha Royal <sneharoyaladdagiri@gmail.com>
2025-12-25 18:31:08 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,25 +1,20 @@
-import React, { lazy, Suspense }from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Signup from './SignUp';
-import Login from './Login';
-import Welcome from './Welcome';
+import Signup from './SignUp'
+import Login from './Login'
+import Welcome from './Welcome'
 import Dashboard from './Dashboard';
 import ProtectedRoute from './ProtectedRoute';
 import "./App.css";
 
-const Profile = lazy(() => import("./Profile"));
-
-
 function App() {
-  return (
+   
+       return (
     <Router>
-        <Suspense fallback={<h2 style={{ textAlign: "center" }}>Loading...</h2>}>
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/welcome" element={<Welcome />} />
-
-        {/* Protected Route */}
         <Route
           path="/dashboard"
           element={
@@ -28,18 +23,10 @@ function App() {
             </ProtectedRoute>
           }
         />
-           <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
       </Routes>
-        </Suspense>
     </Router>
   );
+  
 }
 
-export default App;
+export default App
